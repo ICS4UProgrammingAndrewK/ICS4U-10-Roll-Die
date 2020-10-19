5252f35aade897fe757c1f96e65974de810c81cd
Ubuntu <ubuntu@ip-172-31-19-228.ec2.internal>
2020-10-19 03:07:52 +0000

    completed

---
+++ RollDie.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * Class RollDie
+ */
+ 
+public class RollDie {
+  
+  public static void main(String[] args) {
+    
+    public static void RollDie(int maxValue) { 
+      
+    //This read the input from the console
+    Scanner userInput = new Scanner(System.in);
+    
+    System.out.println("Please enter the max value for the random number:");
+      
+    if (userChoice = ""){
+      System.out.println("Random number is:" + )
+    } else (){
+        System.out.println("Invalid input, please enter a number")
+      }
+    }
+  }
+}

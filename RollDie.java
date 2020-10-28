83bdf874afb5ad2db7efeef7bb954cb1c99bb026
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-10-28 12:41:31 -0400

    Update RollDie.java

--- RollDie.java
+++ RollDie.java
@@ -1,25 +1,43 @@
 import java.util.Random;
 import java.util.Scanner;
 /**
- * Class RollDie
- */
+*
+* class RollDie
+*
+* @author  Andrew Kuekam
+* @version 1.0
+* @since   2020-18-09
+*/
  
 public class RollDie {
   
-  public static void main(String[] args) {
-    
-    public static void RollDie(int maxValue) { 
+  //Declare var
+  static int maxVal;
+  int num = 1;
+  
+    public static void main(String[] args) {
       
-    //This read the input from the console
-    Scanner userInput = new Scanner(System.in);
+      //procedure
+      GenerateRandNum(maxVal);
+    }  
     
-    System.out.println("Please enter the max value for the random number:");
+    public static void GenerateRandNum(int maxVal) {
       
-    if (userChoice = ""){
-      System.out.println("Random number is:" + )
-    } else (){
-        System.out.println("Invalid input, please enter a number")
-      }
-    }
-  }
+      //This read the input from the console
+      Scanner usermaxInput = new Scanner(System.in);
+      
+      System.out.println("Please enter the max value for the random number:");
+      
+      //This generate a number between 1 and max
+      Random randomNum = new Random();
+      
+      //get userinput to max val
+      int maxval = usermaxInput.nextInt();
+      
+      //number between 0 and 49
+      int num = randomNum.nextInt(maxVal);
+      
+      //Display the num
+      System.out.println(" The random number is " + num);
+    }  
 }
